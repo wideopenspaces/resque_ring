9303e68e5c958c31c0f33df59b086b7656ff4474
Jacob Stetser <jake@wideopenspac.es>
2013-11-06 15:50:05 -0700

    test redis with travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,14 +13,7 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
-require 'minitest/hell'
 
 require 'minitest-spec-context'
 
 require 'mocha/setup'
-
-# Uses the in-memory registry to replace RedisRegistry for testing
-# RedisRegistry will NOT work with minitest/hell
-#
-# TODO: Add a test switch to test with redis
-Resque::Plugins::Resqued::RedisRegistry = Resque::Plugins::Resqued::Registry
