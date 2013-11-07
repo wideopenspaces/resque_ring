71e4d93eec94dc45b12af76165c01bcf2c6fab94
Jacob Stetser <jake@wideopenspac.es>
2013-11-07 11:52:29 -0700

    Initial doc for RedisRegistry

--- lib/resqued/redis_registry.rb
+++ lib/resqued/redis_registry.rb
@@ -7,17 +7,30 @@ module Resque
         PREFIX = 'resqued'
 
         extend Forwardable
-        # Redis can handle these methods directly
         def_delegators :@redis, :get, :set, :sadd, :srem, :incr, :decr
 
+        # @param redis_options [Hash] a hash of options for the redis instance
+        # @option redis_options [String] :host the server where redis is running
+        # @option redis_options [Integer, String] :port the port redis is running on
         def initialize(redis_options)
           @redis = Redis.new(redis_options)
         end
 
+        # Deletes all our keys associated with namespace from Redis.
+        # Useful in tests and when starting fresh.
+        # @param namespace [String] usually {WorkerGroup#name}
         def reset!(namespace)
           @redis.keys(_key(namespace, '*')).each { |k| @redis.del(k) }
         end
 
+        # Registers a new {Worker} instance into Redis by storing its
+        # PID, incrementing the total count of workers, recording the
+        # time of spawn and setting a key to block spawning until expired.
+        # @param name     [String] usually {WorkerGroup#name}
+        # @param pid      [Integer] the PID of the {Worker} process
+        # @param options  [Hash] a hash containing additional options
+        # @option options [Integer] :delay How long spawning is blocked.
+        #   this information comes from {WorkerGroup#wait_time}
         def register(name, pid, options = {})
           atomically do |multi|
             multi.sadd  _key(name, 'worker_list'), localize(pid)
@@ -27,6 +40,11 @@ module Resque
           end
         end
 
+        # De-registers a {Worker} instance identified by pid from Redis by
+        # decrementing worker_count and removing the worker from the list
+        # of workers
+        # @param name [String]  usually {WorkerGroup#name}
+        # @param pid  [Integer] the the PID of the {Worker} process
         def deregister(name, pid)
           atomically do |multi|
             multi.decr  _key(name, 'worker_count')
@@ -34,14 +52,23 @@ module Resque
           end
         end
 
+        # Gets the members of a Redis Set
+        # @param name   [String]  usually {WorkerGroup#name}
+        # @param focus  [String]  the redis key for the Set
+        # @return [Array] the members of the Set
         def list(name, focus)
           @redis.smembers _key(name, focus)
         end
 
+        # Gets the contents of a simple Redis key
+        # @param name   [String]  usually {WorkerGroup#name}
+        # @param focus  [String]  the redis key
+        # @return       [String]  the contents of the key
         def current(name, focus)
           get _key(name, focus)
         end
 
+        # @yieldparam multi [Redis::Multi] a Redis.multi context (for atomic operations)
         def atomically(&block)
           @redis.multi { |multi| yield multi }
         end
