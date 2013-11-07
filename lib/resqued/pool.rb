5e281ec51673d2d020df401218850a13ae623905
Jacob Stetser <jake@wideopenspac.es>
2013-11-06 20:58:00 -0700

    Initial doc for Pool

--- lib/resqued/pool.rb
+++ lib/resqued/pool.rb
@@ -4,51 +4,103 @@ module Resque
       class Pool
         extend HattrAccessor
 
+        # @!attribute [r] worker_group
+        #   @return [WorkerGroup] {WorkerGroup} for this Pool
+        # @!attribute [r] workers
+        #   @return [Array] Array of Workers managed
+        #     by this pool
         attr_reader   :worker_group, :workers
+
+        # @!attribute [r] min
+        #   @return [Integer] minimum # of workers
+        #     available at all times
+        # @!attribute [r] max
+        #   @return [Integer] max # of workers to spawn
         hattr_reader  :options, :first_at, :global_max, :max, :min
 
+        # @param options [Hash] options for this Pool
         def initialize(options)
           @workers = []
           @worker_group = options.delete(:worker_group)
           @options = defaults.merge(options)
         end
 
+        # Spins workers up or down as required
         def manage!
           despawn_if_necessary
           spawn_if_necessary
         end
 
+        # Removes a worker from the {Registry}
+        # associated with {#worker_group}
+        # @param worker [Worker] the worker to be removed
         def deregister(worker)
           worker_group.registry.deregister(worker_group.name, worker.pid)
         end
 
+        # Adds a worker to the list of {#workers} and
+        # then adds it to the {Registry}
+        # associated with {#worker_group}
+        # @param worker [Worker] the worker to be added
         def register(worker)
           @workers << worker
           options = worker_group.manager.delay ? { delay: worker_group.manager.delay } : {}
           worker_group.registry.register(worker_group.name, worker.pid, options)
         end
 
+        # @return [Integer] number of current workers for this Pool
+        #   fetched from the {Registry}
         def current_workers
           worker_group.registry.current(worker_group.name, :worker_count).to_i
         end
 
+        # @return [Array] pids of the current worker processes as
+        #   fetched from the {Registry}
         def worker_processes
           worker_group.registry.list(worker_group.name, :worker_list) || []
         end
 
+        # @return [String] a string containing the time the last
+        #   {Worker} was spawned
         def last_spawned
           worker_group.registry.current(worker_group.name, :last_spawned)
         end
 
+        # @return [Boolean] true if a key called
+        #   'spawn_blocked' returns a value of 1
+        #   (meaning it hasn't expired in Redis)
         def spawn_blocked?
           worker_group.registry.current(worker_group.name, :spawn_blocked) == '1'
         end
 
+        # @return [Boolean] true if {#spawn_blocked?}
+        #   returns false
         def able_to_spawn?
           !spawn_blocked?
         end
 
+        # @return [Boolean] true if {#worker_processes} spawned
+        #   is greater than {#min}
+        def min_workers_spawned?
+          worker_processes >= min
+        end
+
+        # @return [Boolean] true if {#worker_processes} spawned
+        #    is less than {#max}
+        def room_for_more?
+          worker_processes < max
+        end
+
         private
+
+        def defaults
+          {
+            first_at:    1,
+            global_max:  0,
+            max:         5,
+            min:         1
+          }
+        end
 
         def despawn_if_necessary
           despawn! if worker_group.wants_to_remove_workers?
@@ -65,19 +117,6 @@ module Resque
           spawn! if worker_group.wants_to_add_workers? && room_for_more?
         end
 
-        def defaults
-          {
-            first_at:    1,
-            global_max:  0,
-            max:         5,
-            min:         1
-          }
-        end
-
-        def min_workers_spawned?
-          worker_processes >= min
-        end
-
         def spawn!
           worker = Resque::Plugins::Resqued::Worker.new(worker_options)
           worker.start!
@@ -86,10 +125,6 @@ module Resque
 
         def worker_options
           worker_group.worker_options.merge({ pool: self })
-        end
-
-        def room_for_more?
-          worker_processes < max
         end
       end
     end
