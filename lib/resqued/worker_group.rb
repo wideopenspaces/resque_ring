8f2383647d95a99599e75d01568f5a6f11ba9017
Jacob Stetser <jake@wideopenspac.es>
2013-11-06 20:37:24 -0700

    Initial doc for WorkerGroup

--- lib/resqued/worker_group.rb
+++ lib/resqued/worker_group.rb
@@ -4,9 +4,26 @@ module Resque
       class WorkerGroup
         extend HattrAccessor
 
+        # @!attribute [r] name
+        #   @return [String] name of the WorkerGroup
+        # @!attribute [r] manager
+        #   @return [Manager] {Manager} class for this WorkerGroup
+        # @!attribute [r] queues
+        #   @return [Hash{String => Queue}] a hash containing
+        #     the {Queue} instances managed by this
+        #     WorkerGroup, organized by name
         attr_reader :name, :manager, :queues
+
+        # @!attribute [r] threshold
+        #   @return [Integer] the number of queue items
+        #     needed before spawning more workers
+        # @!attribute [r] remove_when_idle
+        #   @return [Boolean] whether or not to reduce
+        #     worker capacity when idle. Defaults to false
         hattr_reader :options, :spawn_rate, :threshold, :wait_time, :remove_when_idle
 
+        # @param name [String] a name for this WorkerGroup
+        # @param options [Hash] options for this WorkerGroup
         def initialize(name, options = {})
           @name = name.to_s
           @manager = options.delete(:manager)
@@ -15,50 +32,76 @@ module Resque
           @options = defaults.merge(options)
         end
 
+        # A list of environment variables used when
+        # spawning a {Worker Worker}
+        # @return [Hash] ENV variable names & values
         def environment
           @env ||= @options[:spawner][:env]
         end
 
+        # Instructs a {Pool} to manage its workers
         def manage!
           pool.manage!
         end
 
+        # @return [Pool] the pool of workers for this WorkerGroup
         def pool
           @pool ||= Resque::Plugins::Resqued::Pool.new(@options[:pool].merge(worker_group: self))
         end
 
+        # @return [Boolean] true if all associated queues are empty
         def queues_are_empty?
           queues_total == 0
         end
 
+        # @return [Integer] sum of sizes of all associated queues
         def queues_total
           queues.values.map(&:size).reduce(:+)
         end
 
+        # @return [Registry] the {Registry} associated with this
+        #   WorkerGroup's {Manager}
         def registry
           manager.registry
         end
 
+        # @return [Array] an array of strings for building the
+        #   spawning command
         def spawn_command
           @spawn_command ||= @options[:spawner][:command]
         end
 
+        # collects an [Array] of spawn command elements
+        # while replacing a placeholder with a list
+        # of associated {Queue Queues} concatenated into
+        # a String
+        # @return [Array] an array of strings used by
+        #   the {Pool} when spawning a {Worker}
         def spawner
           spawn_command.collect { |c| c.gsub('{{queues}}', "QUEUES=#{queues.map(&:to_s).join(',')}") }
         end
 
+        # @return [Boolean] true if total items in all queues
+        #   is greater than {#threshold} and the {Pool} is
+        #   ready to spawn a new worker
         def wants_to_add_workers?
           queues_total >= threshold && pool.able_to_spawn?
         end
 
+        # @return [Boolean] true if all queues are empty
+        #   and {#remove_when_idle} returns true
         def wants_to_remove_workers?
           remove_when_idle && queues_are_empty?
         end
 
+        # @return [String] the working directory for
+        #   the spawner command
         def work_dir
           @work_dir ||= @options[:spawner][:dir]
         end
 
+        # @return [Hash] options for the {Worker} that
+        #   {Pool} will spin up on request
         def worker_options
           { spawner: spawner, env: environment, cwd: work_dir }
         end
