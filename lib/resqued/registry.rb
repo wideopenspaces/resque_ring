38f25148df2b704cfb43525a913f3bfe0c68e9ae
Jacob Stetser <jake@wideopenspac.es>
2013-11-07 11:25:27 -0700

    Initial doc for Registry module

--- lib/resqued/registry.rb
+++ lib/resqued/registry.rb
@@ -3,8 +3,14 @@ module Resque
     module Resqued
       module Registry
         HOST = `hostname`.strip.freeze
+
+        # @return [String] the local machine's hostname
         def host; HOST; end
 
+        # Adds the host to the given value to namespace it
+        # by server
+        # @param value [String,Integer] the value to be localized
+        # @return [String] the given value, with {#host} prepended
         def localize(value)
           "#{host}:#{value}"
         end
