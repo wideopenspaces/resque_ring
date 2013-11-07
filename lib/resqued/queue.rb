646fddfdda601792db9625496e8dd715850aebb5
Jacob Stetser <jake@wideopenspac.es>
2013-11-07 11:03:19 -0700

    Initial doc for Queue

--- lib/resqued/queue.rb
+++ lib/resqued/queue.rb
@@ -2,22 +2,35 @@ module Resque
   module Plugins
     module Resqued
       class Queue
-        attr_reader :name, :worker_group, :store
+        # @return [String] the name of this Queue in Resque
+        attr_reader :name
 
+        # @return [WorkerGroup] the {WorkerGroup} that owns this Queue
+        attr_reader :worker_group
+
+        # @return [Resque] an instance of {Resque}
+        attr_reader :store
+
+        # @param options [Hash] the options, including name,
+        #  parent {WorkerGroup} and {Resque} instance
+        #  for this queue
         def initialize(options = {})
           @name = options.delete(:name)
           @worker_group = options.delete(:worker_group)
           @store = options.delete(:store)
         end
 
+        # @return [Integer] the size of this queue in Resque
         def size
           store.size(name) rescue 0
         end
 
+        # @return [String] the name of the queue
         def to_s
           name
         end
 
+        # @return [String] a simple representation of the instance
         def inspect
           "Queue:#{object_id}:#{name}:#{size}"
         end
