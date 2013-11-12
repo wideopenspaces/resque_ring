d51dc94025b6ba01dad8a1666391e8937f429403
Jacob Stetser <jake@wideopenspac.es>
2013-11-11 20:50:43 -0700

    Remove MemoryRegistry
    Don't need to maintain a second Registry class now that we are mocking
    Redis.

--- spec/memory_registry_base_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'resqued/memory_registry'
-
-describe Resque::Plugins::Resqued::MemoryRegistry do
-  let(:registry) { Resque::Plugins::Resqued::MemoryRegistry.new }
-  it 'knows its own host' do
-    registry.host.must_equal(`hostname`.strip)
-  end
-end
